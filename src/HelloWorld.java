f8eaa879d16aca54958c5c74ea4ed39ff59ad18a
@song-cheng-liu <2304420467@qq.com>
2023-04-19 17:17:12 +0800

    测试上传项目到Githup

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author lsc
+ * #Description HelloWorld
+ * #Date: 2023/4/19 17:07
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
