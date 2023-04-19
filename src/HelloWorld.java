8751c38ac915da3f3fe163c46a26829c7b71530a
@song-cheng-liu <2304420467@qq.com>
2023-04-19 18:17:03 +0800

    利用newbranch添加文件

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("add source");
+        System.out.println("use newBranch PUSH");
     }
 }
